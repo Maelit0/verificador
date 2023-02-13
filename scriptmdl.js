881fd68f9af693ac47ea339b4d14a63525c47950
Ismael Renan <ismaelrenan091@gmail.com>
2023-02-13 16:04:30 -0300

    verifier

---
+++ scriptmdl.js
@@ -0,0 +1,19 @@
+function carregar() {
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem')
+    let data = new Date()
+    //let hora = data.getHours()
+    let hora = 15
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manhã.png'
+        document.body.style.background = '#ff8f01'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#de2c0f'
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#1d2627'
+    }
+}
+
